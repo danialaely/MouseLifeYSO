3cb2efebe33c2b44b0e7caf011ce4d18162ca948
DMahad <mahaddevx@gmail.com>
2025-08-25 18:06:09 +0500

    commit

---
+++ Assets/Scripts/SkinsManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinsManager : MonoBehaviour
+{
+
+    public static SkinsManager INSTANCE { get; private protected set; }
+
+    [SerializeField] Material PlayerMaterial;
+    [SerializeField] List<Skin> Skins = new List<Skin>();
+
+    private void Awake()
+    {
+        if (INSTANCE != null)
+            Destroy(gameObject);
+        else
+        {
+            INSTANCE = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+}
+
+[Serializable]
+public enum Payment
+{
+    IAP,
+    Cheese,
+    Gems,
+    Free,
+}
+
+[Serializable]
+public class Skin
+{
+    public string id;
+    public string name;
+    public bool isPurchased;
+    public bool isSelected;
+    public Texture texture;
+    public Payment payment;
+    public int price;
+}
